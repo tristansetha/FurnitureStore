3d43c5af4b37782e7c516bcf87f1517be36a3a6e
Guest User <Guest@epicodus-79.local>
2019-04-24 13:01:04 -0700

    functioning

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 
 $(document).ready(function(){
   $('#furniture').click(function(){
+
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://it771mq5n2.execute-api.us-west-2.amazonaws.com/production/furniture`;
@@ -19,13 +20,13 @@ $(document).ready(function(){
 
     promise.then(function(response){
       let body = JSON.parse(response);
-      // let bodyData = body.data;
-      console.log(body);
-      if (body.length === 0) {
+      let bodyData = body.body.data;
+      console.log(body.body);
+      if (bodyData.length === 0) {
         $('#showFurniture').text('No furniture')
       } else {
-        for (let i = 0; i < body.length; i++) {
-          $('#showFurniture').append('<li>${body.data</li>');
+        for (let i = 0; i < bodyData.length; i++) {
+          $('#showFurniture').append(`<li>${bodyData[i].name}</li>`);
         }
       }
     })

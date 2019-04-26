8b30a32c87ff68041fe3b8c652e9edadd59aaf16
ts <tristanasetha@gmail.com>
2019-04-26 16:12:10 -0700

    added reset button

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import './styles.css';
 import $ from 'jquery';
 
 $(document).ready(function(){
-    $("#showByTypeId").click(function(){
+    $("#showByTypeId").click(function(event){
+      event.preventDefault();
     let selectedOption = $("#selectType").val();
     // console.log(selectedOption);
 
@@ -36,40 +37,7 @@ $(document).ready(function(){
       }
     })
   })
+  $("#reset").click(function(){
+  location.reload();
 })
-
-
-  // $('#showByType').submit(function(){
-  //   // event.preventDefault();
-  //   let selectedOption = $("#selectType").val();
-  //   console.log()
-  //   let promise = new Promise(function(resolve, reject) {
-  //     let request = new XMLHttpRequest();
-  //     let url = `https://it771mq5n2.execute-api.us-west-2.amazonaws.com/production/furniture`;
-  //     request.onload = function() {
-  //       if (this.status === 200) {
-  //         resolve(request.response);
-  //       } else {
-  //         reject(Error(request.statusText));
-  //       }
-  //     }
-  //     request.open("GET", url, true);
-  //     request.send();
-  //   })
-
-  //   promise.then(function(response){
-  //     let body = JSON.parse(response);
-  //     let bodyData = body.body.data;
-  //     console.log(bodyData);
-  //     if (bodyData.length === 0) {
-  //       $('#showFurniture').text('No furniture')
-  //     } else {
-  //       for (let i = 0; i < bodyData.length; i++) {
-  //         if (bodyData[i].type === selectedOption) {
-  //           $('#showFurniture').append(`<li>${bodyData[i].name}</li>`);
-  //           $('#showFurniture').append(`<li>${bodyData[i].type}</li>`);
-  //         }
-  //       }
-  //     }
-  //   })
-  // })
+})
